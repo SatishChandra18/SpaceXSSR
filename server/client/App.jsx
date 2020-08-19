58b72f0cc4e5cad8ff47dac690b345fe7e3c4908
SatishChandra18 <satishchandra.navi@gmail.com>
2020-08-19 16:38:05 +0530

    fixing heroku issue

--- server/client/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Launches from './component/launches';
-
-export const App = ({ initialData }) => (
-    <div className="container mt-5">
-        <div className='heading'>SPACEX</div>
-        <Launches launches={initialData} />
-    </div>
-)
