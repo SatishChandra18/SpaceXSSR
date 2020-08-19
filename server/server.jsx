a12f6bc077a40901a2f291cc0c4b4b9984f982af
SatishChandra18 <satishchandra.navi@gmail.com>
2020-08-19 12:21:35 +0530

    fixing heroku issue

--- server/server.jsx
+++ server/server.jsx
@@ -6,26 +6,12 @@ import { readFileSync } from 'fs';
 import "isomorphic-fetch";
 
 import { App } from '../client/App';
-// import { getData, modifyAnswerUpvotes } from './database';
 
 const app = new express();
 const port = 7777;
 
 app.use(compression());
 app.use(express.static("dist"));
-
-/* app.get("/data", async (_req, res) => {
-
-    res.json(await getData());
-
-});
-
-app.get("/vote/:answerId", (req, res) => {
-
-    const { query, params } = req;
-    modifyAnswerUpvotes(params.answerId, parseInt(query.increment));
-
-}); */
 
 app.get("/", async ( _req, res )=>{
 
